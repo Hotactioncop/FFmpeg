488a850119ecfe3603fe587112a2faa321a28c62
Araz Iusubov <Primeadvice@gmail.com>
2024-08-01 15:56:19 +0200

    avutil: add hwcontext_amf.

--- libavutil/pixdesc.c
+++ libavutil/pixdesc.c
@@ -2119,6 +2119,10 @@ static const AVPixFmtDescriptor av_pix_fmt_descriptors[AV_PIX_FMT_NB] = {
         .name = "cuda",
         .flags = AV_PIX_FMT_FLAG_HWACCEL,
     },
+    [AV_PIX_FMT_AMF_SURFACE] = {
+        .name = "amf",
+        .flags = AV_PIX_FMT_FLAG_HWACCEL,
+    },
     [AV_PIX_FMT_AYUV64LE] = {
         .name = "ayuv64le",
         .nb_components = 4,
