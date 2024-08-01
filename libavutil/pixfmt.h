d2cc973541a08dcd85fc7a86bc3ddf527cc19a27
Araz Iusubov <Primeadvice@gmail.com>
2024-08-01 15:56:19 +0200

    avutil: add hwcontext_amf.

--- libavutil/pixfmt.h
+++ libavutil/pixfmt.h
@@ -439,6 +439,11 @@ enum AVPixelFormat {
      */
     AV_PIX_FMT_D3D12,
 
+    /**
+     * HW acceleration through AMF. data[0] contain AMFSurface pointer
+     */
+    AV_PIX_FMT_AMF_SURFACE,
+
     AV_PIX_FMT_NB         ///< number of pixel formats, DO NOT USE THIS if you want to link with shared libav* because the number of formats might differ between versions
 };
 
